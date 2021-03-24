55c96cccb3c30ffb7ddb1c70fc8b610aebae3a46
spahiuredon <redon.spahiu@stud.kbw.ch>
2021-03-24 15:47:17 +0100

    added mp4 file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import ReactPlayer from 'react-player'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Video extends Component {
+    render () {
+        return (
+        <div className='player-wrapper'>
+            <ReactPlayer
+            className='react-player fixed-bottom'
+            url= 'videos/ipv4.mp4'
+            width='100%'
+            height='100%'
+            controls = {true}
+            volume = '1'
+
+
+            />
+        </div>
+        )
+    }
 }
 
-export default App;
+export default Video;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
+import Video from './App';
 import App from './App';
+import Video from './App'
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<Video />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
